03a0c8a1c5e039a3bc74227fccde1d4d5e4b02c6
TK <takonewa.v@gmail.om>
2023-06-14 08:14:24 +0200

    fix nfc manager

--- lib/services/emv_platform_service.dart
+++ lib/services/emv_platform_service.dart
@@ -1,12 +1,14 @@
 import 'dart:io';
+import 'package:nfc_manager/nfc_manager.dart';
 import 'package:platform_device_id/platform_device_id.dart';
 
 class EmvPlatformService {
   static Future<bool> isNfcEnabled() async {
-    /*if (Platform.isAndroid) {
+    if (Platform.isAndroid) {
      return (await NfcManager.instance.isAvailable());
-    }*/
-    return (true);
+    }else{
+      return false;
+    }
   }
 
   static Future<String?> getDeviceId() async {
