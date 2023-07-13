f6029103fd3bb90c23c3e860970d09b56aace890
TK <takonewa.v@gmail.om>
2023-07-13 16:23:53 +0200

    update

--- lib/services/session_service.dart
+++ lib/services/session_service.dart
@@ -49,7 +49,7 @@ class SessionService extends ChangeNotifier {
   }
 
   Future<Uri?> createPaymentSession(
-      EMVPaymentRequest payment, String deviceId, dynamic action) async {
+      EMVPaymentRequest payment, String deviceId) async {
     dynamic meta = jsonDecode(payment.meta);
     meta['deviceId'] = deviceId;
     meta['original_session_id'] = payment.sessionId;
